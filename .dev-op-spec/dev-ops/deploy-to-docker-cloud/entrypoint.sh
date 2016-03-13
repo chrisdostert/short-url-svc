110420639ac0d98c7937ee8f220e72a0f649c0b4
Chris Dostert <chris.dostert@live.com>
2016-03-12 17:54:53 -0800

    updated docker cloud

--- .dev-op-spec/dev-ops/deploy-to-docker-cloud/entrypoint.sh
+++ .dev-op-spec/dev-ops/deploy-to-docker-cloud/entrypoint.sh
@@ -3,13 +3,13 @@ SERVICE_VERSION=$(cat target/VERSION)
 
 updateDockerCloudService () {
 
-tutum service set --sync --image ${DOCKER_REPOSITORY_NAME}:${SERVICE_VERSION} short-url-svc.${DOCKERCLOUD_STACK_NAME}
+docker-cloud service set --sync --image ${DOCKER_REPOSITORY_NAME}:${SERVICE_VERSION} short-url-svc.${DOCKERCLOUD_STACK_NAME}
 
 }
 
 redeployDockerCloudService () {
 
-tutum service redeploy --sync short-url-svc.${DOCKERCLOUD_STACK_NAME}
+docker-cloud service redeploy --sync short-url-svc.${DOCKERCLOUD_STACK_NAME}
 
 }
 
